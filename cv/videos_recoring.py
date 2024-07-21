946a919d6cc4700cb3a8e0c5453565fea1ed0dbc
aayush7379 <aayushprajapti789@gamil.com>
2024-07-21 12:27:43 +0530

    face detectionand hand detection and scraping

--- cv/videos_recoring.py
+++ cv/videos_recoring.py
@@ -1,18 +1,21 @@
 import cv2
 
-video = cv2.VideoCapture(r"D:\note 10\Screenrecorder-2022-11-18-21-46-08-305.mp4")
+video = cv2.VideoCapture(0)
+codec = cv2.VideoWriter_fourcc(*'DIVX')
+output = cv2.VideoWriter("output.mp4",codec,20.0,(640,480))
 while True:
     status, frame = video.read()
-    print(status,frame)
+   # print(status,frame)
     if not status:
         break
     # make changes from this line 
     newframe =frame*10
-    bw = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    rgb = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    output.write(gray)
     cv2.imshow("video frame", frame)
     cv2.imshow("new video frame", newframe)
     if cv2.waitKey(1) == 27:
         break
+    output.release()
     video.release()
     cv2.destroyAllWindows()
